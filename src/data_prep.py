c80a303fb6acad36edbc25d12e8b81edbce8812e
Daksh Sharma <dakshsharmabhatt@gmail.com>
2025-09-13 16:34:44 +0100

    Adjusted into a function to import

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,24 +1,19 @@
 import os
 import numpy as np
 from PIL import Image
-from sklearn.model_selection import train_test_split
 
-image_size = (28,28)
-labels = ['circle', 'square', 'triangle']
-X, y = [], []
-
-for label in labels:
-    folder = os.path.join('../data', label)
-    for filename in os.listdir(folder):
-        filepath = os.path.join(folder, filename)
-        img = Image.open(filepath).convert('L') # grayscaling image
-        img = img.resize(image_size)            # resizing image
-        arr = np.array(img).flatten() / 255.0   # create a 1D array of each pixel, stored as a float from 0 (black) to 1 (white)
-        X.append(arr)
-        y.append(label)
-
-X = np.array(X)
-y = np.array(y)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=1)
-print(f'Training samples: {len(X_train)}, Test Samples: {len(X_test)}')
+def convert_images(image_size, labels):
+    X = []
+    y = []
+    for label in labels:
+        folder = os.path.join('data', label)
+        for filename in os.listdir(folder):                                 # Iterate over images in circle, square and traingle folders
+            filepath = os.path.join(folder, filename)
+            with Image.open(filepath).convert('L') as img:                  # Convert the images to greyscale
+                img = img.resize(image_size)                                      # Decrease the size
+                arr = np.array(img).flatten() / 255.0                       # Create a 1D array of each pixel, stored as a float from 0 (black) to 1 (white)
+            X.append(arr)
+            y.append(label)
+    X = np.array(X)                                                         # Convert to numpy arrays as these are more efficient
+    y = np.array(y)
+    return X, y
